a2713d816148b7476fef23301769dd9ddcd6dbf9
Shambhavi Punja <shambhavipunja.03@gmail.com>
2016-01-26 13:08:14 -0800

    Api connection and subscription

---
+++ googlefitmodule/src/main/java/com/example/shambhavipunja/googlefitmodule/LSGoogleFitConnectionListener.java
@@ -0,0 +1,9 @@
+package com.example.shambhavipunja.googlefitmodule;
+
+/**
+ * Created by shambhavipunja on 1/26/16.
+ */
+public interface LSGoogleFitConnectionListener {
+    public void ConnectionStatus(String Error);
+    public void SubscribeStatus(String Error);
+}
